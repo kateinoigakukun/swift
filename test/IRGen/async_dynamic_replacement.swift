f0239f24b5abb25ae5b8a1c046ec9d76372d1376
Yuta Saito <ysaito22@apple.com>
2023-10-07 22:13:32 +0000

    [test][IRGen] Fix async_dynamic_replacement.swift for non-musttail

--- test/IRGen/async_dynamic_replacement.swift
+++ test/IRGen/async_dynamic_replacement.swift
@@ -13,17 +13,17 @@ internal func _replacement_number() async -> Int {
 
 // rdar://78284346 - Dynamic replacement should use musttail
 //                   for tail calls from swifttailcc to swifttailcc
-// CHECK-LABEL: define {{.*}} swifttailcc void @"$s25async_dynamic_replacement01_C7_numberSiyYaFTI"
-// CHECK: musttail call swifttailcc void
+// CHECK-LABEL: define {{.*}} swift{{(tail)?}}cc void @"$s25async_dynamic_replacement01_C7_numberSiyYaFTI"
+// CHECK: {{(must)?}}tail call swift{{(tail)?}}cc void
 // CHECK-NEXT: ret void
 
 public func calls_number() async -> Int {
   await number()
 }
 
-// CHECK-LABEL: define {{.*}}swifttailcc void @"$s25async_dynamic_replacement32indirectReturnDynamicReplaceableSi_S6ityYaKF"(ptr {{.*}}%0, ptr swiftasync %1)
+// CHECK-LABEL: define {{.*}}swift{{(tail)?}}cc void @"$s25async_dynamic_replacement32indirectReturnDynamicReplaceableSi_S6ityYaKF"(ptr {{.*}}%0, ptr swiftasync %1)
 // CHECK: forward_to_replaced:
-// CHECK: musttail call swifttailcc void {{.*}}(ptr noalias nocapture %0, ptr swiftasync {{.*}})
+// CHECK: {{(must)?}}tail call swift{{(tail)?}}cc void {{.*}}(ptr noalias nocapture %0, ptr swiftasync {{.*}})
 public dynamic func indirectReturnDynamicReplaceable() async throws -> (Int, Int, Int, Int, Int, Int, Int) {
     return (0, 0, 0, 0, 0, 0, 0)
 }
